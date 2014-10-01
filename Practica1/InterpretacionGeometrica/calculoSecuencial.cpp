10658a7eacf56e10e5d5e7742c114211c961a3fb
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2014-10-01 23:45:01 +0200

    Añadido control de tiempo

--- Practica1/InterpretacionGeometrica/calculoSecuencial.cpp
+++ Practica1/InterpretacionGeometrica/calculoSecuencial.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "fun_tiempo.h"
+#include "fun_tiempo.c"
 using namespace std;
 
-unsigned long m = 100000000;
+unsigned long m = 100000;
 
 double funcion(double x){
 	return 4.0/(1+x*x);
@@ -10,9 +12,14 @@ double funcion(double x){
 int main(){
 
 	double suma;
-
+	
+	struct timespec inicio=ahora();
+	
 	for(int i=0; i<m; i++)
 		suma += funcion((i+0.5)/m);
+	
+	struct timespec fin=ahora();
 
-		cout <<  suma/m;
+		cout << suma/m;
+		cout << "Tiempo transcurrido == " << duracion(&inicio, &fin) << " seg. " << endl;
 }
